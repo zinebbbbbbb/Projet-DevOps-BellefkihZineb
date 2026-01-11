448052eb5d18210089c0a0d5ed8500c2ca5b77b4
zinebbbbbbb <zineb.belfkih@emsi-edu.ma>
2026-01-11 11:59:35 +0100

    Add file1

--- hh.java
+++ hh.java
@@ -1,5 +1,7 @@
 public class hh{
     public static void main(String[] args) {
-        System.out.println( "Bonjour et bon courage dans votre projet en DevOps");
+        System.out.println( "Bonjour et bon courage dans votre projet en DevOps branch dev");
     }
 }
+
+git commit -m "Add file1"
